0e40290f252c98f4d30fd8d1d0f8ba3d6a73d6b2
TORRES SILVA Gabriel <gabriel.torres@soprasteria.com>
2024-02-13 15:43:49 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,80 @@
+const { MongoClient, ObjectId} = require('mongodb');
+const { createObjectCsvWriter } = require('csv-writer');
+const fs = require('fs');
+const {progressBar, zipFolder, findImages} = require('./utils.js');
+
+// Redefined console.error function for a red output
+console.error = function(message){
+  process.stderr.write('\x1b[31m' + message + '\x1b[0m\n');
+}
+
+console.info = function(message){
+  process.stderr.write('\x1b[34m' + message + '\x1b[0m\n');
+}
+
+const auditName = process.argv[2];
+if(auditName == undefined){
+  console.error("No audit name provided. For example, to retrieve the audit 'test' use 'npm start test'");
+  process.exit(1);
+}
+
+async function exportToCSV() {
+  const uri = 'mongodb://localhost:27017';
+  const dbName = 'pwndoc';
+  const collectionName = 'audits';
+  const csvFilePath = 'Findings.csv';
+  const client = new MongoClient(uri);
+
+  const csvWriter = createObjectCsvWriter({
+    path: `${auditName}/${csvFilePath}`,
+    header: [
+      {id:'title',title:'Name'},
+      {id:'description',title:'Description'},
+      {id:'observation',title:'Observation'},
+    ]
+  });
+  try {
+    await client.connect();
+    if(!fs.existsSync(auditName)){
+      fs.mkdirSync(auditName);
+    }
+    const database = client.db(dbName);
+    const collection = database.collection(collectionName);
+    console.info("=== Retreiving data from MongoDB ===");
+    console.info(progressBar(0.2));
+    let cursor = await collection.findOne({name: auditName},{projection: {_id:0,findings:1}});
+    if(cursor == null){
+      console.clear();
+      console.error(`No audit found with the name : ${auditName}`);
+    }else{
+      cursor = cursor.findings;
+      let index = 1;
+      console.clear();
+      console.info("=== Uploading images ===");
+      console.info(progressBar(0.4));
+      cursor.forEach(async (item) => {
+        item.description = await findImages(item.title, index, item.description, database);
+        item.observation = await findImages(item.title, index, item.observation, database);
+        console.log(item.observation);
+        await csvWriter.writeRecords(cursor);
+        index++;
+      })
+    }
+    console.clear();
+    console.info("=== Creating CSV file & Zip file ===");
+    console.info(progressBar(0.6));
+    zipFolder(`${auditName}`, `${auditName}.zip`).then(() => {
+      console.clear();
+      console.info("=== Job done ===");
+      console.info(progressBar(1));
+      }).catch((error) => {
+          console.error(error);
+    });
+  } catch (error) {
+    console.error('Error:', error);
+  } finally{
+    await client.close();
+  }
+}
+
+exportToCSV();
