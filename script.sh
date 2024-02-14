292867815fd6013bd55df87d51d9ce2cfd38686b
Ubuntu <azureuser@PwnDoc.gk2dkex0n2debbgs2aradmodnd.ax.internal.cloudapp.net>
2024-02-14 15:30:19 +0000

    Added multiple recipients functionnality

--- script.sh
+++ script.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
+. ./config.file
 
 encrypt_file() {
     local file="$1"
-    local recipients="$2"
     local relative_path="${file#$directory/}"
     local output_path="$output_folder/$relative_path"
 	mkdir -p "$(dirname "$output_path")"
-    	gpg --encrypt --recipient "$recipients" --output "$output_path.gpg" "$file"
-    	echo gpg --encrypt --recipient "$recipients" --output "$output_path.gpg" "$file"
+    	gpg --encrypt --recipient "$firstRecipient" --recipient "$secondRecipient" --recipient "$thirdRecipient" --output "$output_path.gpg" "$file"
 }
 
 encrypt_directory() {
     local directory="$1"
-    local recipients="$2"
     for file in "$directory"/*; do
-        echo $file
 	    if [[ -f "$file" ]]; then
-            encrypt_file "$file" "$recipients"
+            encrypt_file "$file"
         elif [[ -d "$file" ]]; then
-            encrypt_directory "$file" "$recipients"
+            encrypt_directory "$file"
         fi
     done
 }
 
 main() {
     local directory="$1"
-    local recipients="$2"
     if [[ ! -d "$directory" ]]; then
         echo "Error: $directory is not a valid directory."
         exit 1
@@ -39,12 +35,12 @@ main() {
     done
     
     # Encrypt files in the input directory and its sub-directories
-    encrypt_directory "$directory" "$recipients"
+    encrypt_directory "$directory"
 }
 
 # Check for correct number of arguments
-if [[ "$#" -ne 2 ]]; then
-    echo "Usage: $0 <directory> <recipients>"
+if [[ "$#" -ne 1 ]]; then
+    echo "Usage: $0 <directory>"
     exit 1
 fi
 
@@ -55,4 +51,4 @@ mkdir -p "$output_folder"
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Call the main function with the provided arguments
-main "$1" "$2"
+main "$1"
